bb706e4e7a78d090806c03b9161a237ab703d396
Jerinji2016 <jerinji2016@gmail.com>
2023-02-12 04:53:32 +0530

    custom image loader

--- lib/ui/image_details_view.dart
+++ lib/ui/image_details_view.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
+import 'package:shimmer/shimmer.dart';
 
 import '../modals/image_data.dart';
 
-class ImageDetailsView extends StatelessWidget {
+class ImageDetailsView extends StatefulWidget {
   final ImageData image;
 
   const ImageDetailsView({
@@ -12,26 +13,106 @@ class ImageDetailsView extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<ImageDetailsView> createState() => _ImageDetailsViewState();
+}
+
+class _ImageDetailsViewState extends State<ImageDetailsView> with SingleTickerProviderStateMixin {
+  late final AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Hero(
-        tag: image.key,
-        child: PhotoView(
-          imageProvider: NetworkImage(
-            image.hdUrl,
+        tag: widget.image.key,
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: PhotoView(
+            imageProvider: NetworkImage(
+              widget.image.hdUrl,
+            ),
+            loadingBuilder: (context, imageChunkEvent) {
+              debugPrint("ImageDetailsView.build: $imageChunkEvent");
+
+              double? progress;
+              if (imageChunkEvent != null) {
+                progress = imageChunkEvent.cumulativeBytesLoaded / imageChunkEvent.expectedTotalBytes!;
+                debugPrint("ImageDetailsView.build: progress: $progress");
+
+                if (progress == 1.0) {
+                  controller.forward();
+                }
+              }
+
+              return Material(
+                color: Colors.transparent,
+                child: Center(
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      Positioned(
+                        bottom: 20.0,
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: Column(
+                            children: [
+                              Text("jerin"),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(4.0),
+                                  ),
+                                  child: LinearProgressIndicator(
+                                    value: progress,
+                                    color: Colors.orange,
+                                    backgroundColor: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Shimmer.fromColors(
+                        highlightColor: Colors.white,
+                        baseColor: Colors.grey[500]!,
+                        period: const Duration(milliseconds: 800),
+                        child: Container(
+                          margin: const EdgeInsets.only(right: 0),
+                          height: 350,
+                          width: 270,
+                          decoration: BoxDecoration(
+                            color: Colors.black26,
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+            errorBuilder: (context, url, error) {
+              return const Text("Failed to load Image");
+            },
           ),
-          loadingBuilder: (context, imageChunkEvent) {
-            debugPrint("ImageDetailsView.build: $imageChunkEvent");
-            return const Center(
-              child: SizedBox.square(
-                dimension: 30.0,
-                child: CircularProgressIndicator(),
-              ),
-            );
-          },
-          errorBuilder: (context, url, error) {
-            return const Text("Failed to load Image");
-          },
         ),
       ),
     );
