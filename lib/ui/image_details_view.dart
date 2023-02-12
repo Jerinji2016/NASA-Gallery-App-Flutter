c128445c0b20a7c411d1c85568889b604d888550
Jerinji2016 <jerinji2016@gmail.com>
2023-02-12 15:21:15 +0530

    improvised loading widget

--- lib/ui/image_details_view.dart
+++ lib/ui/image_details_view.dart
@@ -27,6 +27,7 @@ class _ImageDetailsViewState extends State<ImageDetailsView> with SingleTickerPr
       vsync: this,
       duration: const Duration(milliseconds: 800),
     );
+    controller.value = 1.0;
   }
 
   @override
@@ -38,80 +39,99 @@ class _ImageDetailsViewState extends State<ImageDetailsView> with SingleTickerPr
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Hero(
         tag: widget.image.key,
-        child: Container(
+        child: SizedBox(
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
-          child: PhotoView(
-            imageProvider: NetworkImage(
-              widget.image.hdUrl,
-            ),
-            loadingBuilder: (context, imageChunkEvent) {
-              debugPrint("ImageDetailsView.build: $imageChunkEvent");
-
-              double? progress;
-              if (imageChunkEvent != null) {
-                progress = imageChunkEvent.cumulativeBytesLoaded / imageChunkEvent.expectedTotalBytes!;
-                debugPrint("ImageDetailsView.build: progress: $progress");
-
-                if (progress == 1.0) {
-                  controller.forward();
-                }
-              }
-
-              return Material(
-                color: Colors.transparent,
-                child: Center(
-                  child: Stack(
-                    fit: StackFit.expand,
-                    children: [
-                      Positioned(
-                        bottom: 20.0,
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width,
-                          child: Column(
-                            children: [
-                              Text("jerin"),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(4.0),
-                                  ),
-                                  child: LinearProgressIndicator(
-                                    value: progress,
-                                    color: Colors.orange,
-                                    backgroundColor: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Shimmer.fromColors(
-                        highlightColor: Colors.white,
-                        baseColor: Colors.grey[500]!,
-                        period: const Duration(milliseconds: 800),
-                        child: Container(
-                          margin: const EdgeInsets.only(right: 0),
-                          height: 350,
-                          width: 270,
-                          decoration: BoxDecoration(
-                            color: Colors.black26,
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+          child: AnimatedBuilder(
+            animation: controller,
+            builder: (context, child) => Opacity(
+              opacity: Tween<double>(begin: 0.0, end: 1.0)
+                  .animate(
+                    CurvedAnimation(
+                      parent: controller,
+                      curve: Curves.ease,
+                    ),
+                  )
+                  .value,
+              child: PhotoView(
+                backgroundDecoration: const BoxDecoration(
+                  color: Colors.black,
                 ),
-              );
-            },
-            errorBuilder: (context, url, error) {
-              return const Text("Failed to load Image");
-            },
+                imageProvider: NetworkImage(
+                  widget.image.hdUrl,
+                ),
+                loadingBuilder: (context, imageChunkEvent) {
+                  debugPrint("ImageDetailsView.build: $imageChunkEvent");
+
+                  double? progress;
+                  if (imageChunkEvent != null) {
+                    progress = imageChunkEvent.cumulativeBytesLoaded / imageChunkEvent.expectedTotalBytes!;
+                    debugPrint("ImageDetailsView.build: progress: $progress");
+
+                    if (progress == 1.0) {
+                      controller.forward(from: 0.0);
+                    }
+                  }
+
+                  return Material(
+                    color: Colors.transparent,
+                    child: Center(
+                      child: Stack(
+                        children: [
+                          Shimmer.fromColors(
+                            highlightColor: Colors.white38,
+                            baseColor: const Color(0xFF363636),
+                            period: const Duration(milliseconds: 800),
+                            child: Container(
+                              margin: const EdgeInsets.only(right: 0),
+                              color: Colors.black,
+                            ),
+                          ),
+                          Positioned(
+                            bottom: 30.0,
+                            child: SizedBox(
+                              width: MediaQuery.of(context).size.width,
+                              child: Column(
+                                children: [
+                                  Text(
+                                    widget.image.title,
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 8.0),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                                    child: ClipRRect(
+                                      borderRadius: const BorderRadius.all(
+                                        Radius.circular(4.0),
+                                      ),
+                                      child: LinearProgressIndicator(
+                                        value: progress,
+                                        minHeight: 6.0,
+                                        color: Colors.orange,
+                                        backgroundColor: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                errorBuilder: (context, url, error) {
+                  return const Text("Failed to load Image");
+                },
+              ),
+            ),
           ),
         ),
       ),
