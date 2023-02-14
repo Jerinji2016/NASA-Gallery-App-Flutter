ab11abb73bc07585ab4a7b09b08a864da63da808
Jerinji2016 <jerinji2016@gmail.com>
2023-02-15 04:04:33 +0530

    added sample test to check data read from asset json

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,25 +5,24 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:nasa_gallery/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const NASAGalleryApp(data: ""));
+  test(
+    "Read asset file test",
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+      String data = await rootBundle.loadString(
+        'assets/data.json',
+      );
+      var json = jsonDecode(data);
+      expect(json.runtimeType, List);
+      expect(json.length, greaterThan(0));
+    },
+  );
 }
